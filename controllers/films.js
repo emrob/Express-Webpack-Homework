261271c39651283bb985b4f95c8911aef715aeff
Emma Roberts <emrsixtyseven@gmail.com>
2018-04-16 19:03:21 +0100

    initial setup

---
+++ controllers/films.js
@@ -0,0 +1,32 @@
+//since we don't have a database we'll use our front end models at the moment
+var express = require('express');
+var filmsRouter = new express.Router();
+
+var films = require('../client/src/models/films')();
+var Film = require('../client/src/models/film');
+var Review = require('../client/src/models/review');
+
+filmsRouter.get('/:id', function(req,res){
+  res.json({data: films[req.params.id]});
+});
+
+filmsRouter.get('/', function(req, res) {
+  res.json(films);
+});
+
+filmsRouter.post('/', function(req,res){
+  cats.push(req.body.cat);
+  res.json({data: films});
+});
+
+filmsRouter.put('/:id', function(req, res){
+  films[req.params.id] = req.body.cat;
+  res.json({data: films});
+});
+
+filmsRouter.delete('/:id', function(req,res){
+  films.splice(req.params.id, 1);
+  res.json({data:films});
+});
+
+module.exports = filmsRouter;
